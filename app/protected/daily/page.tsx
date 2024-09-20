7df99d795d9fd6c20b371f92e57f48989244bd0f
Jack Rowe <rowejackj@gmail.com>
2024-09-20 16:13:57 -0400

    initial commit
    removed some template specific code
    
    added basic starting point design for home page
    
    more scaffolding for building on
    
    adjusted layout
    
    added tosater package
    
    added downgrade and upgrade to premium actions
    
    added downgrade and upgrade to premium actions
    
    added downgrade and upgrade to premium actions

---
+++ app/protected/daily/page.tsx
@@ -0,0 +1,21 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function DailyPage() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-3 p-3 sm:p-4 max-w-md mx-auto">
+      <header className="text-center mb-2">
+        <h1 className="text-2xl font-bold">Daily Game Page</h1>
+      </header>
+    </div>
+  );
+}
---
+++ app/protected/page.tsx
@@ -0,0 +1,5 @@
+import ProtectedHomePage from "@/components/pages/protected-home-page";
+
+export default async function ProtectedPage() {
+  return <ProtectedHomePage />;
+}
