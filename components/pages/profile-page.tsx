d4e6d202493dc9bff0ccd4f10beee97d3571f539
Jack Rowe <rowejackj@gmail.com>
2024-09-28 15:48:53 -0400

    added tosater package

--- components/pages/profile-page.tsx
+++ components/pages/profile-page.tsx
@@ -4,17 +4,16 @@ import { redirect } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Alert, AlertDescription } from "@/components/ui/alert";
 import { editProfile } from "@/actions/profile-actions";
 import { PencilIcon, CheckIcon, Zap } from "lucide-react";
 import LoadingSpinner from "../loading-spinner";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function ProfilePage() {
   const user = useProfile();
   const [editingField, setEditingField] = useState<string | null>(null);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
 
   if (user.isLoading) {
     return <LoadingSpinner />;
@@ -38,15 +37,25 @@ export default function ProfilePage() {
         field === "username"
           ? { ...user.data, username }
           : { ...user.data, email };
-
       await editProfile(updatedData);
-      setMessage(
-        `${field.charAt(0).toUpperCase() + field.slice(1)} updated successfully!`
+      toast.success(
+        `${field.charAt(0).toUpperCase() + field.slice(1)} updated successfully!`,
+        {
+          iconTheme: {
+            primary: "black",
+            secondary: "white",
+          },
+        }
       );
       setEditingField(null);
       user.refetch();
     } catch (error) {
-      setMessage(`Failed to update ${field}. Please try again.`);
+      toast.error(`Failed to update ${field}. Please try again.`, {
+        iconTheme: {
+          primary: "red",
+          secondary: "white",
+        },
+      });
     }
   };
 
@@ -58,7 +67,6 @@ export default function ProfilePage() {
     const isEditing = editingField === field;
     const fieldValue = field === "username" ? username : email;
     const setFieldValue = field === "username" ? setUsername : setEmail;
-
     return (
       <div className="space-y-2">
         <label
@@ -103,17 +111,11 @@ export default function ProfilePage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-3 p-3 sm:p-4 max-w-md mx-auto">
+      <Toaster position="bottom-center" reverseOrder={false} />
       <div className="grow">
         <header className="text-center mb-2">
           <h1 className="text-2xl font-bold">Account Page</h1>
         </header>
-
-        {message && (
-          <Alert>
-            <AlertDescription>{message}</AlertDescription>
-          </Alert>
-        )}
-
         <div className="space-y-4">
           {renderField("username", user.data.username)}
           {renderField("email", user.data.email, false)}
